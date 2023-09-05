f05dbfe546daef35225f551ae29159f9acffacec
jurandircod <114868156+jurandircod@users.noreply.github.com>
2023-09-05 10:34:31 -0300

    Site de agendamento
    Site para agendar locais

---
+++ Agendamento/js/script.js
@@ -0,0 +1,25 @@
+var agendarBtn = document.getElementById('anfiteatro');
+var overlay = document.getElementById('overlay');
+var fecharBtn = document.getElementById('fechar-btn');
+
+agendarBtn.addEventListener('click', function () {
+    overlay.style.display = 'block'; // Exibe a tela de sobreposição ao clicar no botão "Agendar"
+});
+
+fecharBtn.addEventListener('click', function () {
+    overlay.style.display = 'none'; // Oculta a tela de sobreposição ao clicar no botão "Fechar"
+});
+
+
+//mudar nomes local 2
+
+
+
+
+
+
+var local_4 = document.getElementById('');
+// Define os eventos de clique nos botões
+
+
+
